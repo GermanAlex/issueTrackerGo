6db52e8bdc69e11c9902abb0005a15a8483d0b8d
Istomin German <needwolf@gmail.com>
2022-05-12 00:45:05 +1000

    chg pckg name and add service.go

--- cmd/service/service.go
+++ cmd/service/service.go
@@ -1,5 +1,33 @@
 package service
 
-func main() {
+import (
+	"fmt"
+	"issueTackerGo/pkg/storage"
+	"issueTackerGo/pkg/storage/postgres"
+	"log"
+	"os"
+)
 
+// Интерфейс БД.
+var db storage.Interface
+
+func main() {
+	var err error
+	pwd := os.Getenv("dbpass")
+	if pwd == "" {
+		os.Exit(1)
+	}
+	connstr :=
+		"postgres://postgres:" +
+			pwd + "@ubuntu-server.northeurope.cloudapp.azure.com/tasks"
+	// присвоение переменной типа интерфейс конкретной реализации БД
+	db, err = postgres.New(connstr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	tasks, err := db.Tasks(0, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(tasks)
 }
